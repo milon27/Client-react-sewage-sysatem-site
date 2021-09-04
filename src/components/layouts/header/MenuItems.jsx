295eb81e651d0d92b3e1444dd8af4ddd3c26390d
milon27 <mdjahidulislammilon@gmail.com>
2021-09-04 19:52:16 +0600

    add exact in navlink

--- src/components/layouts/header/MenuItems.jsx
+++ src/components/layouts/header/MenuItems.jsx
@@ -6,20 +6,20 @@ export default function MenuItems() {
     return (
         <>
             <li >
-                <NavLink to={URL.HOME} activeClassName="nav-active">
+                <NavLink exact to={URL.HOME} activeClassName="nav-active">
                     Home
                 </NavLink>
 
             </li>
 
             <li >
-                <NavLink to={URL.ABOUT} activeClassName="nav-active">
+                <NavLink exact to={URL.ABOUT} activeClassName="nav-active">
                     About Company
                 </NavLink>
             </li>
 
             <li className="deeper parent">
-                <NavLink to={URL.ABOUT} className="sub-sub-menu" activeClassName="nav-active ">
+                <NavLink exact to={URL.ABOUT} className="sub-sub-menu" activeClassName="nav-active ">
                     System And Design
                 </NavLink>
                 <ul class="nav-child unstyled small">
@@ -39,18 +39,18 @@ export default function MenuItems() {
 
 
             <li >
-                <NavLink to={URL.ABOUT} activeClassName="nav-active">
+                <NavLink exact to={URL.ABOUT} activeClassName="nav-active">
                     Products
                 </NavLink>
             </li>
 
             <li >
-                <NavLink to={URL.ABOUT} activeClassName="nav-active">
+                <NavLink exact to={URL.ABOUT} activeClassName="nav-active">
                     News
                 </NavLink>
             </li>
             <li className="deeper parent">
-                <NavLink to={URL.ABOUT} activeClassName="nav-active">
+                <NavLink exact to={URL.ABOUT} activeClassName="nav-active">
                     Certification
                 </NavLink>
                 <ul class="nav-child unstyled small">
